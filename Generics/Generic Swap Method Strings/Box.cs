c6602cec7580fa11a200372463eac6b47c721f84
matanovm <matanov.martin@gmail.com>
2019-02-25 00:28:30 +0200

    Add Ex Genereic Swap Method Strings

---
+++ Generics/Generic Swap Method Strings/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace Generic_Swap_Method_Strings
+{
+	class Box<T>
+	{
+		private T value;
+
+		public Box(T value)
+		{
+			this.value = value;
+		}
+
+		public override string ToString()
+		{
+			return $"{value.GetType().FullName}: {value}";
+		}
+	}
+}
