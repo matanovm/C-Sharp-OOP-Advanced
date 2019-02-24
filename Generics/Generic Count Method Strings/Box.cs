e4834d5ed0326f699778ec62517024f0229503ba
matanovm <matanov.martin@gmail.com>
2019-02-25 00:40:39 +0200

    Add Ex. Generic Count Method Strings

---
+++ Generics/Generic Count Method Strings/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Count_Method_Strings
+{
+	public class Box<T>
+		where T : IComparable<T>
+	{
+		public Box(List<string> items)
+		{
+			this.Items = items;
+		}
+
+		public List<string> Items { get; set; }
+
+		public override string ToString()
+		{
+			return $"{this.Items.GetType()}: {this.Items}";
+		}
+
+		internal int GetGreaterthan(T element)
+		{
+			int count = 0;
+
+			foreach (var item in this.Items)
+			{
+				if (item.CompareTo(element) > 0)
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+	}
+}
---
+++ Generics/Generic Count Method Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Count_Method_Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			List<string> messages = new List<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				string message = Console.ReadLine();
+				messages.Add(message);
+			}
+
+			string element = Console.ReadLine();
+
+			Box<string> box = new Box<string>(messages);
+			int result = box.GetGreaterthan(element);
+			Console.WriteLine(result);
+		}
+	}
+}
